259f9e9efc1a34f81f46b9a25f891c80c1907873
Andreanog27 <decasn.2781@gmail.com>
2025-11-24 11:43:47 -0300

    temp.
    novo temp.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from main.views import TaskList
+from  . import views
 
 urlpatterns = [
-    path("", TaskList.as_view(), name='task_list'),
+    path("testest", TaskList.as_view(), name='task_list'),
+    path('', views.task_list, name= 'task_list_função'),
+    path('tarefas_concluidas', views.task_concluido, name='task_concluida') 
 ]
--- main/views.py
+++ main/views.py
@@ -10,4 +10,32 @@ class TaskList(ListView):
     template_name = 'tasks/task_list.html'
     context_object_name = 'tarefas'
     
-    # views - urls - template
+
+
+def task_list(request):
+    tarefas = Task.objects.all()
+    context = {
+        "tarefas":tarefas,
+        "titulo_pagina": 'Minhas Tarefas'
+    }
+    
+    return render(request, 'tasks/task_list.html', context)    
+
+
+    
+def task_concluido(request):
+    a = 4
+    b = 5
+    c = a + b
+        
+    tarefas = Task.objects.filter(concluida=1)
+    context ={
+        "tarefas": tarefas,
+        "titulo_pagina": 'Minhas Tarefas Concluidas',
+        'c':c
+        
+            
+        }
+    return render(request, 'tasks/task_list.html', context) 
+        
+        
